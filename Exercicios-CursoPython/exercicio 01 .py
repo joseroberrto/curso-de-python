54c5773b741abae0f12b46b742ad2bf2c53ae8cb
Jose Roberto <joserobertoapd@gmail.com>
2025-08-08 23:22:54 -0300

    Update exercicio 01 .py

--- Exercicios-CursoPython/exercicio 01 .py
+++ Exercicios-CursoPython/exercicio 01 .py
@@ -3,7 +3,7 @@ n1 = int(input("Digite um número :"))
 n2 = int(input("Digite outro número :"))
 soma = n1 + n2 # soma
 
-#fim do programa e um charme
-print('-=-' * 20)
+
+print('-=-' * 20) #linha divisoria
 print(f'A soma entre {n1} e {n2} é  {soma}')
 print('-=-' * 20)
